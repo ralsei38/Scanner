ebe99273bc2749ffaca48a91e64f318bf6834e0d
ralsei38 <fontana.theo.me@gmail.com>
2022-07-16 11:28:43 +0200

    [ADD] get_nb_host_support()

--- Scanner.py
+++ Scanner.py
@@ -1,10 +1,12 @@
+from curses import keyname
+import pdb
 from scapy.all import sr1, srp,IP,ICMP,Ether
 
 class Ip:
-    is_up = False
     def __init__(self, ip_str, netmask_str) -> None:
         self.ip_str = ip_str
         self.netmask_str = netmask_str
+        self.is_up = False
     
     def next(self) -> None:
         ip_arr = [int(part) for part in self.ip_str.split('.')]
@@ -16,19 +18,40 @@ class Ip:
                 ip_arr[i] = 0
                 continue
         self.ip_str = ".".join([str(part) for part in ip_arr])
-    
+
     def __str__(self) -> str:
         return self.ip_str
 
 class Network:
     def __init__(self, ip) -> None:
         self.ip = ip
-
+        # self.nb_host = self.get_nb_host()
+    
     def ping(self, ip: Ip) -> bool:
         pckt = Ether()/IP(dst=str(ip))/ICMP(type=8, code=0)
-        print(pckt.show())
         pqt = sr1(pckt)
         return pqt
     
+    def get_nb_host_support(self) -> int:
+        """
+            How much hosts can this ip range could welcome
+        """
+        netmask_bin = ''.join([ format(int(part), '08b') for part in self.ip.netmask_str.split('.')])
+        n_free_bits = 0
+        for i in reversed(netmask_bin):
+            if int(i) == 0:
+                n_free_bits += 1
+            else:
+                break
+        return 2**n_free_bits-1 # last IP broadcast, real number is 2**nb_free_bits-2
+
+
     def ping_scan(self) -> list:
+        '''
+        255.255.255.0
+        1111 1111. 1111 1111. 1111 1111. 0000 0000
+        '''
         pass
+
+network = Network(Ip('192.168.1.0', '255.255.255.224'))
+print(network.get_nb_host_support())
