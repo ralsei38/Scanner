0d50ccaae8bf5c6b24e9f2b5d4fd99f9a48fbca2
ralsei38 <fontana.theo.me@gmail.com>
2022-07-17 20:54:29 +0200

    testing ip format DONE

--- tests/scanner_test.py
+++ tests/scanner_test.py
@@ -6,6 +6,20 @@ import pytest
 
 
 # TEST IP CLASS
-def test_init():
-    ip = scanner.Ip("192.168.1.0", "255.255.255.0")
-    assert(str(ip) == "192.168.1.0")
+def test_init_wrong_ip_format():
+    with pytest.raises(ValueError):
+        scanner.Ip("192.168.1.0.0", "255.255.255.0")
+    with pytest.raises(ValueError):
+        scanner.Ip("192.168.1.0", "255.255.255.0.0")
+    with pytest.raises(ValueError):
+        scanner.Ip("192..168.1.0", "255.255.255..0")
+    with pytest.raises(ValueError):
+        scanner.Ip("1some4", "255.255.255.0")
+    with pytest.raises(ValueError):
+        scanner.Ip("a.a.a.b", "255.255.255..0")
+
+def test_init_wrong_ip_type():
+    with pytest.raises(TypeError):
+        scanner.Ip(14, "255.255.255.0") 
+        scanner.Ip([], "255.255.255.0") 
+        scanner.Ip({}, "255.255.255.0") 
