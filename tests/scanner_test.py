be83e7951089d89a3ecc6e5e7f949380eb922320
ralsei38 <fontana.theo.me@gmail.com>
2022-07-18 13:23:55 +0200

    testing ping on a single host

--- tests/scanner_test.py
+++ tests/scanner_test.py
@@ -1,5 +1,8 @@
+from array import array
 import os
 import sys
+
+from scanner.scanner import Network
 sys.path.append('scanner')
 import scanner
 import pytest
@@ -24,5 +27,12 @@ def test_init_wrong_ip_type():
         scanner.Ip([], "255.255.255.0") 
         scanner.Ip({}, "255.255.255.0")
 
-# def test_ping():
-#     assert(scanner.Network(scanner.Ip("192.168.1.0", "255.255.255.0")).ping(scanner.Ip("192.168.1.13", "255.255.255.0")))
+def test_ping():
+    assert(scanner.Network(scanner.Ip("192.168.1.0", "255.255.255.0")).ping("192.168.1.0"))
+
+def test_ping_scan():
+    ip = scanner.Ip("192.168.1.0", "255.255.255.0")
+    network = Network(ip)
+    result = network.ping_scan() 
+    assert(isinstance(result, array()))
+    assert("192.168.1.0" in result)
