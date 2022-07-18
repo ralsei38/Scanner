9bbb1cafdade729b7b75fe8b25d3d4b945d47d3e
ralsei38 <fontana.theo.me@gmail.com>
2022-07-18 09:44:50 +0200

    making sure bad coverage is not valid (CI)

--- tests/scanner_test.py
+++ tests/scanner_test.py
@@ -24,5 +24,10 @@ def test_init_wrong_ip_type():
         scanner.Ip([], "255.255.255.0") 
         scanner.Ip({}, "255.255.255.0")
 
+def test_idiotic():
+    """
+    just making sure that CI will spot bad coverage
+    """
+    assert(scanner.Ip("192.168.1.0", "255.255.255.0") == scanner.Ip("192.168.1.0", "255.255.255.0"))
 # def test_ping():
 #     assert(scanner.Network(scanner.Ip("192.168.1.0", "255.255.255.0")).ping(scanner.Ip("192.168.1.13", "255.255.255.0")))
