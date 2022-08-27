e8374542509203dea33e83c2ba35b479c39f52ad
ralsei38 <fontana.theo.me@gmail.com>
2022-08-28 00:56:28 +0200

    first view implementation

---
+++ scanner/view.py
@@ -0,0 +1,32 @@
+from pyfiglet import Figlet
+import os
+
+class View():
+    def __init__(self):
+        self.f = Figlet()
+    
+    def welcome(self):
+        os.system('cls||clear')
+        print(self.f.renderText('ScapyScan'))
+    
+    def list_focus(self, focus: dict) -> int:
+        user_focus = None
+        
+        for key in focus:
+            print(key, '->', focus[key])
+        user_focus = input()
+        return user_focus
+    
+    def list_actions(self, actions: dict) -> int:
+        user_action = None
+        
+        for key in actions:
+            print(key, '->', actions[key])
+        user_action = input()
+        return user_action
+    
+    def ip_get(self):
+        ip_str = input("ip: ")
+        netmask_str = input("netmask: ")
+        return (ip_str, netmask_str)
+        
